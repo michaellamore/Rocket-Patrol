14d0542b8b5992469887b9a668d88f990b94a833
Michael Lamore <michael.remorin2023@gmail.com>
2022-04-07 10:16:41 -0700

    FINISHED ROCKET PATROL

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,6 +5,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
     scene.add.existing(this);
     this.isFiring = false;
     this.moveSpeed = 2;
+
+    this.sfxRocket = scene.sound.add('sfx_rocket');
   }
 
   update() {
@@ -19,8 +21,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
     }
 
     // fire button
-    if (Phaser.Input.Keyboard.JustDown(keyF)){
+    if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
       this.isFiring = true;
+      this.sfxRocket.play();
     }
 
     // if fired, move up
@@ -33,5 +36,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
       this.isFiring = false;
       this.y = game.config.height - borderUISize - borderPadding;
     }
+  }
+
+  reset() {
+    this.isFiring = false;
+    this.y = game.config.height - borderUISize - borderPadding;
   }
 }
